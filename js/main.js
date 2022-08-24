463421a0b4b7017caf20ffc27ec77db3c9b7a862
abdurashiidov <abdurashiidov18@gmail.com>
2022-08-24 09:39:56 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 let elForm = document.querySelector(".todo-form"); 
-let elList = document.querySelector(".todo-list");
+let elList = document.querySelector(".todo-list");  
 let elInput = document.querySelector(".todo-input");
 let elTemplate = document.querySelector("#todo-item--template").content;
 
 const todosArr = []
+// ! function delete
 function daleteBtn(e){
       const todoId = e.target.dataset.id
       let findindex = todosArr.findIndex((elem)=> elem.id == todoId)
       todosArr.splice(findindex,1)
       renderTodo(todosArr, elList)
+
+    //   elInput.value=null
 }
+// !
+
+
+// ! function edit
+function editBtn(e) {
+    // const todoId = e.target.dataset.id
+    // let findIndex = todosArr.findIndex((elem) => elem.id == todoId)
+    
+}
+// !
+
+//! function checked
+
+//!
+
 
 function renderTodo(todoArr,element){
       element.innerHTML = null
@@ -17,13 +35,17 @@ function renderTodo(todoArr,element){
             const cloneTemplate = elTemplate.cloneNode(true)
             const elTitle = cloneTemplate.querySelector('.todo-item-complete-text');
             const elDaleteBtn = cloneTemplate.querySelector('.todo-item-delete-btn');
+            const elEditBtn = cloneTemplate.querySelector('.todo-item-edit-btn');
             const elCheckBox = cloneTemplate.querySelector('.todo-input-complete');
 
             elTitle.textContent = todo.text,
             elCheckBox.checked = todo.isCompeted,
-            elDaleteBtn.dataset.id = todo.id
 
-            elDaleteBtn.addEventListener('click',daleteBtn)
+            elDaleteBtn.dataset.id = todo.id
+            elEditBtn.dataset.id = todo.id
+
+            elDaleteBtn.addEventListener('click', daleteBtn)
+            elEditBtn.addEventListener('click', editBtn)
 
             element.appendChild(cloneTemplate)
       })
@@ -38,6 +60,6 @@ elForm.addEventListener('submit',(event)=>{
             isCompeted:false
 
       })
-      renderTodo(todosArr,elList)
+      renderTodo(todosArr, elList)
       elInput.value=null
 })
